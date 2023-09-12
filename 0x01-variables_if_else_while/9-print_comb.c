c37759b48b29a6032eb0bcb5d9c203b1defa1f27
tijjan1 <ibnahmadtijjani@gmail.com>
2023-09-12 16:49:07 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,13 +6,14 @@
  * Return: always 0
  */
 int main(void)
+
 {
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
 		if (i == 9)
-			putcharr(i + '0');
+			putchar(i + '0');
 		else
 		{
 			putchar(i '0');
