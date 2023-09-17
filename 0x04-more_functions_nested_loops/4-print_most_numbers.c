2cfde808085d9e1dfcdba01552c9b9a05ddf70ea
tijjan1 <ibnahmadtijjani@gmail.com>
2023-09-16 23:42:28 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - func checks for a digit (though 9)
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		if (c != 50)
+		{
+			if (c != 52)
+			{
+				_putchar(c);
+			}
+		}
+	}
+	_putchar('\n');
+}
