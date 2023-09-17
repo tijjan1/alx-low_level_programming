c729c710c571f3827493b2e0ce06193d017c3220
tijjan1 <ibnahmadtijjani@gmail.com>
2023-09-16 22:22:18 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * _ispper -prog checks for uppercase character
+ * @c: Variable text
+ * Retuurn: Always0 0.
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
