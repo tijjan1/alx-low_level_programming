707f9464069961dd8cc4606ef1e4cba3017a4be0
tijjan1 <ibnahmadtijjani@gmail.com>
2023-09-16 23:13:36 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - func checks forche cks for a digit (0 through).
+ * @a: a - var
+ * @b: b - var
+ * Return: Allways 0.
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a + b;
+	return (mul);
+}
